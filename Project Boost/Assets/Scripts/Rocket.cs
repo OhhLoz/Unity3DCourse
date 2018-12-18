9a93ce7eb57f568c2bae2ef019a44c2f7451e9bf
Unknown <laurence.ure@gmail.com>
2018-12-18 00:02:45 +0000

    Completed Lecture 42 - Basic Input Binding

---
+++ Project Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		ProcessInput();
+	}
+
+	private void ProcessInput()
+	{
+		if(Input.GetKey(KeyCode.W))
+		{
+			print("Thrusting");
+		}
+
+		if(Input.GetKey(KeyCode.A))
+		{
+			print("Rotating Left");
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			print("Rotating Right");
+		}
+	}
+}
