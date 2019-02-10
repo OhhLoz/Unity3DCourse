791761204349dc2bf8288e4f9a65582e0e232bfe
Unknown <laurence.ure@gmail.com>
2019-02-10 00:17:47 +0000

    L60 - Moving a platform in the inspector manuall

---
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+	[SerializeField] Vector3 movementVector;
+	[Range(0,1)][SerializeField] float movementFactor;
+
+	Vector3 startPos;
+
+	void Start()
+	{
+		startPos = gameObject.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 offset = movementVector * movementFactor;
+		transform.position = startPos + offset;
+	}
+}
